45fb68b466c76fbc6398620a903e6645c9b81384
tristan-orourke <tristan.orourke@gmail.com>
2022-12-14 12:58:20 -0500

    Complete day 13

--- src/day13.js
+++ src/day13.js
@@ -1,39 +1,41 @@
-import { spread } from "lodash";
-import { all, cond, constant, eq, every, head, identity, isArray, isInteger, map, overArgs, overEvery, pipe, size, split, subtract, tail } from "lodash/fp"
+import { add, all, any, anyPass, apply, concat, cond, constant, eq, every, flatMap, gt, head, identity, invokeArgs, isArray, isEqual, isInteger, lte, map, multiply, overArgs, overEvery, overSome, pipe, size, split, spread, stubTrue, subtract, sum, tail } from "lodash/fp"
 import { log, logFunctionExecution, unspread, wrapWithLogger } from "./util";
+import input from "./input/day13";
 
 // compare functions: if left is less than right, return -1
 // compare functions: if left is greater than right, return +1
 
 const compareIntegers = subtract;
-const isEmptyOrUndefined = x => eq(undefined, x) || size(x) === 0;
+const isUndefined = eq(undefined)
+const isEmptyArray = x => isArray(x) && size(x) === 0;
 const listWrap = x => [x];
 
 const allTrue = unspread(all(identity));
 
 function compareItems(left, right) {
-  console.log("compare inputs", left, right);
-  if (isEmptyOrUndefined(left) && isEmptyOrUndefined(right)) {
-    console.log("compare lists empty, output 0")
+  if (isEmptyArray(left) && isEmptyArray(right)) {
     return 0;
   }
   const compareValue = cond([
-    [overArgs(allTrue, [isInteger, isInteger]), logFunctionExecution("branch 1")(compareIntegers)],
-    [overArgs(allTrue, [isInteger, isArray]), logFunctionExecution("branch 2")(overArgs(compareItems, [listWrap, identity]))],
-    [overArgs(allTrue, [isArray, isInteger]), logFunctionExecution("branch 3")(overArgs(compareItems, [identity, listWrap]))],
-    [overArgs(allTrue, [isEmptyOrUndefined, isEmptyOrUndefined]), logFunctionExecution("branch 4")(constant(0))],
-    [overArgs(allTrue, [isEmptyOrUndefined, isArray]), logFunctionExecution("branch 5")(constant(-1))],
-    [overArgs(allTrue, [isArray, isEmptyOrUndefined]), logFunctionExecution("branch 6")(constant(1))],
-    [overArgs(allTrue, [isArray, isArray]), logFunctionExecution("branch 7")(compareItems)],
+    [overArgs(allTrue, [isInteger, isInteger]), compareIntegers],
+    [overArgs(allTrue, [isInteger, isArray]), overArgs(compareItems, [listWrap, identity])],
+    [overArgs(allTrue, [isArray, isInteger]), overArgs(compareItems, [identity, listWrap])],
+    [overArgs(allTrue, [isUndefined, isUndefined]), constant(0)],
+    [overArgs(allTrue, [isUndefined, stubTrue]), constant(-1)],
+    [overArgs(allTrue, [stubTrue, isUndefined]), constant(1)],
+    [overArgs(allTrue, [isArray, isArray]), compareItems],
   ])(head(left), head(right));
 
-  return compareValue !== 0 ? log("compare output")(compareValue) : compareItems(tail(left), tail(right));
+  return compareValue !== 0 ? compareValue : compareItems(tail(left), tail(right));
 }
 
-// overArgs(console.log, [isInteger, isInteger])(3, [])
-// overArgs(wrapWithLogger(unspread(all)), [isInteger, isInteger])(3, [])
-const c = (compareItems);
-// c([1], [1]);
+const findIndexesWhere = predicateFn => array => {
+  let indexes = [];
+  for (let index = 0; index < array.length; index++) {
+    if (predicateFn(array[index])) indexes.push(index);
+  }
+  return indexes;
+}
 
 const testInput = 
 `[1,1,3,1,1]
@@ -67,4 +69,27 @@ console.log(pipe(
     map(eval),
     spread(compareItems)
   )),
-)(testInput))
+  findIndexesWhere(gt(0)),
+  map(add(1)), // because problem isn't zero-indexed
+  sum
+)(input))
+
+
+
+
+// Q2
+const dividerPackets = [
+  [[2]],
+  [[6]]
+];
+const isDividerPacket = anyPass(map(isEqual, dividerPackets));
+console.log(pipe(
+  split("\n\n"),
+  flatMap(split("\n")),
+  map(eval),
+  concat(dividerPackets),
+  (array => array.sort(compareItems)),
+  findIndexesWhere(isDividerPacket),
+  map(add(1)), // because problem isn't zero-indexed
+  apply(multiply)
+)(input));
